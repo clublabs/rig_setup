cbd745b11ea2cd4770dc860d7709a1d8575c89dc
Michael Chen <chen.michael@clublabs.com>
2021-09-15 10:53:23 -0700

    add programming environment setup

--- README.md
+++ README.md
@@ -12,3 +12,10 @@ copy and paste the following into your terminal:
 curl https://raw.githubusercontent.com/clublabs/rig_setup/master/config_setup.sh | sh
 ```
 
+Programming Environment Configurations
+---------
+
+Copy and paste the following into your terminal:
+```
+sudo curl https://raw.githubusercontent.com/clublabs/rig_setup/master/platform_installation.sh | sh
+```
---
+++ platform_installation.sh
@@ -0,0 +1,59 @@
+# homebrew
+echo '########################################################'
+echo '###              INSTALLING HOMEBREW                 ###'
+echo '########################################################'
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew doctor
+
+# useful stuff
+echo '########################################################'
+echo '###              INSTALLING UTILITIES                ###'
+echo '########################################################'
+echo '---the_silver_searcher---'
+brew install the_silver_searcher
+echo '---jq---'
+brew install jq
+echo '---tree---'
+brew install tree
+echo '---macvim---'
+brew install macvim
+echo '---visual-studio-code---'
+brew install visual-studio-code
+echo '---caffeine---'
+brew install caffeine
+echo '---shiftit---'
+brew install shiftit
+echo '---flycut---'
+brew install flycut
+echo '---google-chrome---'
+brew install google-chrome
+echo '---slack---'
+brew install slack
+echo '---zoom---'
+brew install zoom
+echo '---microsoft-teams---'
+brew install microsoft-teams
+echo '---webex---'
+brew install webex
+echo '---evernote---'
+brew install evernote
+
+# python
+echo '########################################################'
+echo '###               INSTALLING PYTHON                  ###'
+echo '########################################################'
+brew install python3
+
+# node
+echo '########################################################'
+echo '###            INSTALLING NVM AND NODE               ###'
+echo '########################################################'
+brew install nvm
+nvm install --lts
+npm i -g yarn
+
+# vim bundles
+echo '########################################################'
+echo '###            INSTALLING VIM BUNDLES                ###'
+echo '########################################################'
+vim +PluginInstall +qa
